26f7b79edf893034e2e6709378f60c8d0ef308d3
Georgios Manoltzas <g.manoltzas@indice.gr>
2021-03-30 11:35:30 +0300

    update sample host project

--- samples/Indice.Oba.Host/Program.cs
+++ samples/Indice.Oba.Host/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Hosting = Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Indice.Oba.Host
 {
