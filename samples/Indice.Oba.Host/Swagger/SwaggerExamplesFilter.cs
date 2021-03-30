4309474f0ea0b3f25920a25d80e190398c72fd7e
Georgios Manoltzas <g.manoltzas@indice.gr>
2021-03-30 11:12:06 +0300

    Support .NET 5.0

--- samples/Indice.Oba.Host/Swagger/SwaggerExamplesFilter.cs
+++ samples/Indice.Oba.Host/Swagger/SwaggerExamplesFilter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Indice.Psd2.Cryptography;
+﻿using Indice.Psd2.Cryptography;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -11,8 +7,7 @@ namespace Indice.Oba.Host.Swagger
     public class SchemaExamplesFilter : ISchemaFilter
     {
         public void Apply(OpenApiSchema schema, SchemaFilterContext context) {
-
-            if (context.ApiModel.Type == typeof(Psd2CertificateRequest)) {
+            if (context.Type == typeof(Psd2CertificateRequest)) {
                 schema.Example = Psd2CertificateRequest.Example().ToOpenApiAny();
             }
         }
